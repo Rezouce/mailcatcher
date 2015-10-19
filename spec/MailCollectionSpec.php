564e294565df6abe72729143b229ab6f748b20cf
Rezouce <contact@rezouce.net>
2015-10-19 17:41:36 +0200

    Implements the MailCollection.

--- spec/MailCollectionSpec.php
+++ spec/MailCollectionSpec.php
@@ -11,11 +11,75 @@ class MailCollectionSpec extends ObjectBehavior
 
     function let(MailCatcher $mailCatcher)
     {
-        $this->beConstructedWith($mailCatcher);
+        $messages = [
+            [
+                "id" => 1,
+                "sender" => "<test@example.com>",
+                "recipients" => ["<test@example.com>"],
+                "subject" => "Subject 1",
+                "size" => "404",
+                "created_at" => "2015-10-19T09:40:50.000+00:00",
+            ],
+            [
+                "id" => 2,
+                "sender" => "<test@example.com>",
+                "recipients" => ["<test@example.com>"],
+                "subject" => "Subject 2",
+                "size" => "404",
+                "created_at" => "2015-10-19T09:40:50.000+00:00",
+            ],
+        ];
+
+        $this->beConstructedWith($mailCatcher, $messages);
     }
 
     function it_is_initializable()
     {
         $this->shouldHaveType('MailCatcher\MailCollection');
+    }
+
+    function it_should_return_the_number_of_mails()
+    {
+        $this->count()->shouldReturn(2);
+    }
+
+    function it_should_be_traversable()
+    {
+        $this->shouldBeAnInstanceOf('\IteratorAggregate');
+        $this->getIterator()->shouldBeAnInstanceOf('\Traversable');
+    }
+
+    function it_should_return_its_first_mail()
+    {
+        $this->first()->subject()->shouldReturn('Subject 1');
+    }
+
+    function it_should_throw_an_exception_when_trying_to_get_the_first_mail_of_an_empty_collection(MailCatcher $mailCatcher)
+    {
+        $this->beConstructedWith($mailCatcher, []);
+
+        $this->shouldThrow('MailCatcher\MailException')->duringFirst();
+    }
+
+    function it_should_return_its_last_mail()
+    {
+        $this->last()->subject()->shouldReturn('Subject 2');
+    }
+
+    function it_should_throw_an_exception_when_trying_to_get_the_last_mail_of_an_empty_collection(MailCatcher $mailCatcher)
+    {
+        $this->beConstructedWith($mailCatcher, []);
+
+        $this->shouldThrow('MailCatcher\MailException')->duringLast();
+    }
+
+    function it_should_return_its_n_mail()
+    {
+        $this->get(1)->subject()->shouldReturn('Subject 2');
+    }
+
+    function it_should_throw_an_exception_when_trying_to_get_a_mail_not_in_the_collection()
+    {
+        $this->shouldThrow('MailCatcher\MailException')->duringGet(2);
     }
 }
--- src/MailCollection.php
+++ src/MailCollection.php
@@ -2,23 +2,111 @@
 
 namespace MailCatcher;
 
-class MailCollection
+class MailCollection implements \IteratorAggregate
 {
 
     private $mailCatcher;
 
-    public function __construct(MailCatcher $mailCatcher)
+    private $mails = [];
+
+    /**
+     * @param MailCatcher $mailCatcher
+     * @param array $messages
+     */
+    public function __construct(MailCatcher $mailCatcher, array $messages)
     {
         $this->mailCatcher = $mailCatcher;
+
+        foreach ($messages as $message) {
+            $this->mails[] = $this->createMail($mailCatcher, $message);
+        }
     }
 
+    /**
+     * Create a mail from a MailCatcher message.
+     *
+     * @param MailCatcher $mailCatcher
+     * @param array $message
+     * @return Mail
+     */
+    private function createMail(MailCatcher $mailCatcher, array $message)
+    {
+        return new Mail(
+            $mailCatcher,
+            $message['id'],
+            $message['sender'],
+            $message['recipients'],
+            $message['subject'],
+            $message['size'],
+            $message['created_at']
+        );
+    }
+
+    /**
+     * The number of mails.
+     *
+     * @return int
+     */
     public function count()
     {
-        return 2;
+        return count($this->mails);
     }
 
+    /**
+     * Get the first mail.
+     *
+     * @return Mail
+     * @throws MailException
+     */
     public function first()
     {
-        return new Mail($this->mailCatcher, 'id', 'sender', ['recipients'], 'Subject 1', 'size', '2015-10-19');
+        if (empty($this->mails)) {
+            throw new MailException('There is no email in this collection.');
+        }
+
+        return $this->mails[0];
+    }
+
+    /**
+     * Get the last mail.
+     *
+     * @return Mail
+     * @throws MailException
+     */
+    public function last()
+    {
+        if (empty($this->mails)) {
+            throw new MailException('There is no email in this collection.');
+        }
+
+        return end($this->mails);
+    }
+
+    /**
+     * Get the n mail.
+     *
+     * @param int $number
+     * @return Mail
+     * @throws MailException
+     */
+    public function get($number)
+    {
+        if (count($this->mails) < $number + 1) {
+            throw new MailException(
+                sprintf('You tried to get the %s when there is only %s emails.', $number, count($this->mails))
+            );
+        }
+
+        return $this->mails[$number];
+    }
+
+    /**
+     * Get the iterator.
+     *
+     * @return \ArrayIterator
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->mails);
     }
 }
